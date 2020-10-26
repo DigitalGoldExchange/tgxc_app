b88b09ef240805d5b12ca31f7e1a6d72262334d5
laidback_tony <laidback_tony@laidback.co.kr>
2020-10-26 12:32:25 +0900

    경로변경

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 // const BuildConfig = require('react-native-build-config');
 // console.log(BuildConfig.default.BASE_URL);
 //ios
-let baseURL = 'http://localhost:8093';
+// let baseURL = 'http://localhost:8093';
 //android
 // let baseURL = 'http://10.0.2.2:8093';
 
-// let baseURL = 'http://117.52.98.39:8093';
+let baseURL = 'http://117.52.98.39:8093';
 
 
 export default axios.create({
